9ca2213aea0c97eba90b002e1aa61de1c6118e68
dirkliu <liuqi0702@126.com>
2020-02-01 10:54:32 +0800

    优化拖动弹窗，修复不能快速拖动到浏览器边缘的问题！

--- src/lib/directives/drag.js
+++ src/lib/directives/drag.js
@@ -10,18 +10,26 @@ Vue.directive('drag', {
       const disX = e.clientX - el.offsetLeft
       const disY = e.clientY - el.offsetTop
  
-      document.onmousemove = function (e) {
+      document.onmousemove = function (event) {
         // 通过事件委托，计算移动元素的距离
-        const l = e.clientX - disX
-        const t = e.clientY - disY
+        const l = event.clientX - disX
+        const t = event.clientY - disY
 
         // 移动当前元素
         // 必须在视口内
         if (el.clientWidth + l < document.body.clientWidth && l > 0) {
             el.style.left = `${l}px`
+        } else if (el.clientWidth + l >= document.body.clientWidth) {
+            el.style.left = `${document.body.clientWidth - el.clientWidth}px`
+        } else {
+          el.style.left = 0
         }
         if (el.clientHeight + t < document.body.clientHeight && t > 0) {
             el.style.top = `${t}px`
+        } else if (el.clientHeight + t >= document.body.clientHeight) {
+            el.style.top = `${document.body.clientHeight - el.clientHeight}px`
+        } else {
+            el.style.top = 0
         }
       }
  
